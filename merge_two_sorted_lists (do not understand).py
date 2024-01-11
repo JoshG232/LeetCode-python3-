340d33003b9b679dfaa2aa3ac9dd8c87b2f6f94f
Josh Georgiou <jcgeorgiou88@gmail.com>
2024-01-11 21:14:01 +0000

    Merge Two sorted lists gg

---
+++ merge_two_sorted_lists (do not understand).py
@@ -0,0 +1,21 @@
+from  typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        mergedArr = []
+        
+
+            
+
+obj = Solution()
+
+obj.mergeTwoLists([1,2,4],[1,3,4])
