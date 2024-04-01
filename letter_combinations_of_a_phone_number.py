76d2cbd2913dca3f244e6c9ae821c9696f56111e
Josh Georgiou <jcgeorgiou88@gmail.com>
2024-04-01 20:09:27 +0100

    nah

---
+++ letter_combinations_of_a_phone_number.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        print("Hello")
+
+        numbers = [*digits]
+        print(numbers)
+
+
+        array2 = ["a","b","c"]
+        array3 = ["d","e","f"]
+        array4 = ["g","h","i"]
+        array5 = ["j","k","l"]
+        array6 = ["m","n","o"]
+        array7 = ["p","q","r","s"]
+        array8 = ["t","u","v"]
+        array9 = ["w","x","y","z"]
+
+
+
+
+
+obj = Solution()
+
+obj.letterCombinations("23")
